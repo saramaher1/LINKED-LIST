a5767076a775086bffce6937b235a1de398ac31a
Sara Maher <sara.almaher94@gmail.com>
2021-03-08 14:35:51 +0100

    initialize my repo

---
+++ src/test/java/se/sdaproject/LinkedListTests.java
@@ -0,0 +1,28 @@
+package se.sdaproject;
+
+import static org.junit.jupiter.api.Assertions.assertEquals;
+
+import org.junit.jupiter.api.Test;
+
+class LinkedListTests {
+
+    @Test
+    void addsTwoNumbers() {
+        assertEquals(2, 1 + 1, "1 + 1 should equal 2");
+    }
+
+    @Test
+    void addANumberToList() {
+        LinkedList list = new LinkedList();
+        list.add(5);
+        String expected = "LinkedList(5)";
+        assertEquals(list.toString(), expected);
+    }
+
+    @Test
+    void testToStringOfEmptyList() {
+        LinkedList list = new LinkedList();
+        String expected = "LinkedList()";
+        assertEquals(list.toString(), expected);
+    }
+}
